c4bb04bb0e687018180efaa85dbed7d6e67419c1
karan5984 <komalsinghag@gmail.com>
2025-08-17 01:29:23 +0530

    Update RPS_game.py

--- RPS_game.py
+++ RPS_game.py
@@ -1,7 +1,25 @@
 import random
-choice = ["Rock", "Paper", "Scissor","Rock", "Paper", "Scissor","Rock", "Paper", "Scissor"]
-print("Enter number from 1 to 9 to randomly choose your choice from Rock, Paper and Scissor.")
-list = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+def botRPSgame():
+    bot = random.choice(choice)
+    while True:
+        player = input("\nEnter your choice: ")
+        if player not in list:
+            print("Invalid Input!!! Please try again.")
+        else:
+            random.shuffle(choice)
+            p_choice = choice[int(player) - 1]
+            break
+    print("\nResult:")
+    if p_choice == "Scissor" and bot == "Paper":
+        print(f"Congratulations! Player wins for {p_choice} over bot's {bot}!")
+    elif p_choice == "Paper" and bot == "Rock":
+        print(f"Congratulations! Player wins for {p_choice} over bot's {bot}!")
+    elif p_choice == "Rock" and bot == "Scissor":
+        print(f"Congratulations! Player 1 wins for {p_choice} over bot's {bot}!")
+    elif p_choice == bot:
+        print(f"Draw for both {p_choice} choice.")
+    else:
+        print(f"Congratulations! Bot wins for {bot} over Player's {p_choice}")
 def RPSgame():
     while True:
         player1 = input("\nEnter player 1 choice: ")
@@ -21,23 +39,49 @@ def RPSgame():
             break
     print("\nResult:")
     if p1_choice == "Scissor" and p2_choice == "Paper":
-        print(f"Congratulations! Player 1 wins for {p1_choice} over player 2 {p2_choice}!")
+        print(f"Congratulations! Player 1 wins for {p1_choice} over player 2's {p2_choice}!")
     elif p1_choice == "Paper" and p2_choice == "Rock":
-        print(f"Congratulations! Player 1 wins for {p1_choice} over player 2 {p2_choice}!")
+        print(f"Congratulations! Player 1 wins for {p1_choice} over player 2's {p2_choice}!")
     elif p1_choice == "Rock" and p2_choice == "Scissor":
-        print(f"Congratulations! Player 1 wins for {p1_choice} over player 2 {p2_choice}!")
+        print(f"Congratulations! Player 1 wins for {p1_choice} over player 2's {p2_choice}!")
     elif p1_choice == p2_choice:
         print(f"Draw for both players {p1_choice} choice.")
     else:
-        print(f"Congratulations! Player 2 wins for {p2_choice} over Player 1 {p1_choice}")
-RPSgame()
+        print(f"Congratulations! Player 2 wins for {p2_choice} over Player 1's {p1_choice}")
 while True:
-    print("\nDo you want to play again ?")
-    again = str(input("Enter 'y' for yes and 'n' for no: "))
-    if again == "y" or again == "Y":
+    mode = int(input("\nModes:\n1. Single player\n2. Multiplayer\n3. Exit\n\nChoose mode: "))
+    if( mode == 1):
+        choice = ["Rock", "Paper", "Scissor","Rock", "Paper", "Scissor","Rock", "Paper", "Scissor"]
+        print("\nEnter number from 1 to 9 to randomly choose your choice from Rock, Paper and Scissor.")
+        list = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+        botRPSgame()
+        while True:
+            print("\nDo you want to play again ?")
+            again = str(input("Enter 'y' for yes and 'n' for no: "))
+            if again == "y" or again == "Y":
+                botRPSgame()
+            elif again == "n" or again == "N":
+                print("Thanks for playing :)")
+                break
+            else:
+                print("Invalid Input!!! Please try again.")
+    elif (mode == 2):
+        choice = ["Rock", "Paper", "Scissor","Rock", "Paper", "Scissor","Rock", "Paper", "Scissor"]
+        print("\nEnter number from 1 to 9 to randomly choose your choice from Rock, Paper and Scissor.")
+        list = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
         RPSgame()
-    elif again == "n" or again == "N":
-        print("Thanks for playing :)")
+        while True:
+            print("\nDo you want to play again ?")
+            again = str(input("Enter 'y' for yes and 'n' for no: "))
+            if again == "y" or again == "Y":
+                RPSgame()
+            elif again == "n" or again == "N":
+                print("Thanks for playing :)")
+                break
+            else:
+                print("Invalid Input!!! Please try again.")
+    elif (mode == 3):
+        print("...")
         break
     else:
-        print("Invalid Input!!! Please try again.")
+        print("Invalid Input! Try again.")
